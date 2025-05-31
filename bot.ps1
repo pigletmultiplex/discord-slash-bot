4e98aae81b3a7a6c08963c79ea9cfbc99e082c5e
pigletmultiplex <pigletmultiplex@outlook.com>
2025-05-31 22:55:16 +0700

    commit
    commit

--- bot.ps1
+++ bot.ps1
@@ -1,3 +1,7 @@
+Set-Location "C:\Users\Admin\Documents\GitHub\discord-slash-bot"
+
+& "C:\Users\Admin\Documents\GitHub\discord-slash-bot\.venv\Scripts\Activate.ps1"
+
 python.exe -m pip install --upgrade pip
 
 python.exe -m pip install discord.py python-dotenv Pillow PyNaCl sqlalchemy psycopg2
